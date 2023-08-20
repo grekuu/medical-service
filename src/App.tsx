b476db11157b95f4791765cd45242349e1388097
grekuu <gracjanprusik@gmail.com>
2023-08-20 19:57:59 +0200

    feat: header component completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
+import Header from "./pages/components/Header/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
--- src/pages/components/Header/Header.tsx
+++ src/pages/components/Header/Header.tsx
@@ -1,7 +1,29 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 import "./header.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto navigation">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#aboutUs">About Us</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#doctors">Doctors</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+          <Navbar.Collapse className="justify-content-end">
+            <button className="book-btn">Book Online</button>
+          </Navbar.Collapse>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
