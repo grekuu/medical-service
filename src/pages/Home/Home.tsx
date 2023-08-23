1e393408ed3a3063b9dee7a3ffd4105da9858857
grekuu <gracjanprusik@gmail.com>
2023-08-23 20:31:16 +0200

    feat(Home): Home banner finished

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,23 @@
 import "./home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <div className="banner">
+        <div className="banner-content">
+          <div className="banner-left">
+            <h1>Get Quick Medical Service</h1>
+            <p>
+              Start your 14-day free trial. <br /> Get started with referral
+              elementskit today
+            </p>
+            <button>Explore more &#8594;</button>
+          </div>
+          <div className="banner-right"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
