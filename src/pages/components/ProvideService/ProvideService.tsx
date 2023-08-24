dbfc5c17494af84df98ea80987c3e0164910a160
grekuu <gracjanprusik@gmail.com>
2023-08-25 00:01:58 +0200

    feat(ProvideService): component finished

--- src/pages/components/ProvideService/ProvideService.tsx
+++ src/pages/components/ProvideService/ProvideService.tsx
@@ -1,5 +1,6 @@
 import { Container } from "react-bootstrap";
 import "./provideService.scss";
+import { BiSolidCheckCircle } from "react-icons/bi";
 
 const ProvideService = () => {
   return (
@@ -20,6 +21,38 @@ const ProvideService = () => {
           Tosser cheesed off tinkety tonk old fruit arse over give us bell nancy
           boy cup of char say, cuppa brolly.
         </p>
+        <div className="checklist">
+          <div className="checklist-left">
+            <div className="checklist-item">
+              <BiSolidCheckCircle className="checkmark-icon" />
+              <span>On-site lab services</span>
+            </div>
+            <div className="checklist-item">
+              <BiSolidCheckCircle className="checkmark-icon" />
+              <span>Blood Resources Program</span>
+            </div>
+          </div>
+          <div className="checklist-right">
+            <div className="checklist-item">
+              <BiSolidCheckCircle className="checkmark-icon" />
+              <span>Family Planning</span>
+            </div>
+            <div className="checklist-item">
+              <BiSolidCheckCircle className="checkmark-icon" />
+              <span>In vitro Fertilization</span>
+            </div>
+          </div>
+        </div>
+        <div className="divider"></div>
+        <div className="funfact-container">
+          <div className="fact">
+            <div className="number">276+</div>
+            <span>Wining Awards</span>
+          </div>
+          <div className="fact">
+            <div className="number">13k</div> <span>Tests Completed</span>
+          </div>
+        </div>
       </div>
     </Container>
   );
