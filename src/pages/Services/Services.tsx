89a4c23e58ce3925ffeedcfca896c836362e4aa2
grekuu <gracjanprusik@gmail.com>
2023-08-28 17:47:31 +0200

    feat(Services): Services banner

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -1,7 +1,12 @@
+import PageBanner from "../components/PageBanner/PageBanner";
 import "./services.scss";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="services-container">
+      <PageBanner background="/medical-page-banner-4.png" title="Services" />
+    </div>
+  );
 };
 
 export default Services;
