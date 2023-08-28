fb44a8a976b33bd825627c5d72cc1e69356c7d68
grekuu <gracjanprusik@gmail.com>
2023-08-28 16:55:40 +0200

    feat(About): Components imported

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,10 +1,20 @@
 import PageBanner from "../components/PageBanner/PageBanner";
+import ProvideService from "../components/ProvideService/ProvideService";
+import QualifiedDoctors from "../components/QualifiedDoctors/QualifiedDoctors";
+import SpecialService from "../components/SpecialService/SpecialService";
+import Testimonials from "../components/Testimonials/Testimonials";
+import VideoSection from "../components/VideoSection/VideoSection";
 import "./aboutUs.scss";
 
 const AboutUs = () => {
   return (
     <div className="about-us-container">
       <PageBanner background="/medical-breadcamp_about.png" title="About us" />
+      <SpecialService />
+      <ProvideService />
+      <VideoSection />
+      <QualifiedDoctors />
+      <Testimonials deviceType="desktop" />
     </div>
   );
 };
