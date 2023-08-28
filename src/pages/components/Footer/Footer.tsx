58377c13857f6e87868f611ff9c3cb0a4ff0c9d0
grekuu <gracjanprusik@gmail.com>
2023-08-28 16:00:32 +0200

    feat(Footer): footer done

--- src/pages/components/Footer/Footer.tsx
+++ src/pages/components/Footer/Footer.tsx
@@ -1,7 +1,24 @@
+import { Container } from "react-bootstrap";
 import "./footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <Container className="footer-content">
+        <div className="footer-left">
+          Made by{" "}
+          <a href="https://www.linkedin.com/in/gracjan-prusik-b12041251/">
+            Gracjan Prusik
+          </a>
+        </div>
+        <div className="footer-right">
+          <a href="#">Sitemap</a>
+          <a href="#">Terms and Conditions</a>
+          <a href="#">Privacy Policy</a>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
