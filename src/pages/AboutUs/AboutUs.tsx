cc3282993632fd1c521a9198480992c8b169ef83
grekuu <gracjanprusik@gmail.com>
2023-08-28 16:52:51 +0200

    feat(PageBanner): component fully done

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,7 +1,12 @@
+import PageBanner from "../components/PageBanner/PageBanner";
 import "./aboutUs.scss";
 
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <div className="about-us-container">
+      <PageBanner background="/medical-breadcamp_about.png" title="About us" />
+    </div>
+  );
 };
 
 export default AboutUs;
--- src/pages/components/PageBanner/PageBanner.tsx
+++ src/pages/components/PageBanner/PageBanner.tsx
@@ -1,7 +1,28 @@
+import { Container } from "react-bootstrap";
 import "./pageBanner.scss";
+import { GoDotFill } from "react-icons/go";
 
-const PageBanner = () => {
-  return <div>PageBanner</div>;
+interface PageBannerProps {
+  background: string;
+  title: string;
+}
+
+const PageBanner = ({ background, title }: PageBannerProps) => {
+  return (
+    <div
+      className="page-banner-container"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <Container className="content-container">
+        <h6>{title}</h6>
+        <div className="nav-info">
+          <span>Home</span>
+          <GoDotFill className="dot" />
+          <span>{title}</span>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default PageBanner;
