a3de47c15b01579d1d0c3260356932ed8585d8e5
grekuu <gracjanprusik@gmail.com>
2023-08-29 16:56:16 +0200

    feat(FormAppointment): component finished

--- src/pages/components/FormAppointment/FormAppointment.tsx
+++ src/pages/components/FormAppointment/FormAppointment.tsx
@@ -9,29 +9,64 @@ const FormAppointment = () => {
         <h3>
           Tosser cheesed off tinkety tonk old fruit arse over tit give us.
         </h3>
-        <div className="double-input">
-          <input type="text" className="form-input" />
-          <input type="text" className="form-input" />
-        </div>
-        <div className="double-input">
-          <input type="text" className="form-input" />
-          <input type="text" className="form-input" />
-        </div>
-        <div className="double-input">
-          <input type="text" className="form-input" />
-          <input type="text" className="form-input" />
-        </div>
-        <textarea
-          className="form-input textarea-input"
-          name="Your message"
-          placeholder="Message"
-          cols={30}
-          rows={10}
-          spellCheck="false"
-        ></textarea>
-        <button>Book Now &#8594;</button>
+        <form>
+          <div className="double-input">
+            <input
+              type="text"
+              className="form-input"
+              placeholder="Name"
+              required
+            />
+            <input
+              type="email"
+              className="form-input"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="double-input">
+            <input
+              type="tel"
+              className="form-input"
+              placeholder="Phone"
+              required
+            />
+            <input
+              type="number"
+              className="form-input"
+              placeholder="Age"
+              required
+            />
+          </div>
+          <div className="double-input">
+            <input
+              type="date"
+              className="form-input"
+              placeholder="Appointment Date"
+              required
+            />
+            <input
+              type="time"
+              className="form-input"
+              placeholder="12:00 PM"
+              required
+            />
+          </div>
+          <textarea
+            className="form-input textarea-input"
+            name="Your message"
+            placeholder="Message"
+            cols={30}
+            rows={10}
+            spellCheck="false"
+            required
+          ></textarea>
+          <button type="submit">Book Now &#8594;</button>
+        </form>
       </div>
-      <div className="form-right"></div>
+      <div className="form-right">
+        <img src="/medical-appointment.png" alt="image" />
+      </div>
     </Container>
   );
 };
